1a1f210f2c4887d03064fd93b70d98ee5301aed2
janilsorg <janilsorg@gmail.com>
2020-07-01 18:02:33 -0300

    API Commit

---
+++ api/users.js
@@ -0,0 +1,68 @@
+const route = require('express').Router();
+const axios = require('axios');
+
+const apiURL = 'https://api.github.com';
+const token = '192d8338dd1c4bf55137b192ca7524c395fcec0f';
+
+
+
+
+route.get('/', async (req, res) => {
+    let since = req.query.since;
+    try {
+        console.log('started ');
+        let r =  await axios.get(apiURL+'/users?since='+since);
+        
+        let proximo = r.headers.link.split(";")[0].split('=')[1];
+        proximo = proximo.substring(0, proximo.length-1);
+
+        res.set({'proximo': proximo});
+        res.send(r.data) ;
+    } catch (error) {
+        let r = {error: error};
+        res.send(r);
+    }
+});
+
+route.get('/:username/detail', async (req, res) => {
+    let username = req.params.username;
+    let url = apiURL+'/users/'+username;
+    console.log('user name is '+username);
+
+    var headers = {
+        'Authorization': 'Token '+token
+    }
+
+    console.log(headers);
+    try {
+        let r =  await axios.get(url, headers);
+        res.send(r.data) ;
+    } catch (error) {
+        let r = {error: error};
+        res.send(r);
+    }
+    
+});
+
+route.get('/:username/repos', async (req, res) => {
+    
+    let username = req.query.username;
+    
+    let url = apiURL+'/users/'+username+'/repos';
+    console.log(url);
+    try {
+        let r =  await axios.get(url);
+        res.send(r.data) ;
+    } catch (error) {
+        console.log('error');
+        let r = {error: error};
+        res.send(r);
+    }
+    
+});
+
+// https://api.github.com/users/janilsorg/repos
+
+
+
+module.exports = route;
---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+app = express();
+
+const dotenv = require('dotenv');
+
+// Importando rotas
+usersRoute = require('./api/users');
+
+// Recebendo dados do .env
+dotenv.config()
+
+// Caso PORT seja definido no .env considerar, senão, usar 5000
+const PORT = process.env.PORT || 5000;
+
+
+// Middlewares
+app.use(express.json());
+
+// Rotas middlewares
+app.use('/api/users', usersRoute);
+
+app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
