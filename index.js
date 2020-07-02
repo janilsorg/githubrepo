e0c8056f247c97d9f87fb334cae3e2e46e72fabf
janilsorg <janilsorg@gmail.com>
2020-07-02 16:43:05 -0300

    Use Cors

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middlewares
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 
 // Rotas middlewares
 app.use('/api/users', usersRoute);
