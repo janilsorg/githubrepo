59815fcf7b3e566528bd848c04f08e9d233c4414
janilsorg <janilsorg@gmail.com>
2020-07-02 12:01:36 -0300

    Enable cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 app = express();
+const cors = require('cors')
 
 const dotenv = require('dotenv');
 
@@ -15,6 +16,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middlewares
 app.use(express.json());
+app.use(cors());
 
 // Rotas middlewares
 app.use('/api/users', usersRoute);
